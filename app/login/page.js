f426eebfc210612741e15daaaa06af932c74a2d4
fedemanuelian <federicomanuelian@gmail.com>
2025-06-13 19:28:39 -0300

    Create page.js

---
+++ app/login/page.js
@@ -0,0 +1,7 @@
+export default function LoginPage() {
+  return (
+    <main style={{ height: '100vh', backgroundColor: '#000', color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <h1>Login</h1>
+    </main>
+  );
+}
